1b8c55d398c4f6a4590188a645e63b5a31b54a6f
Olivergv1 <136008963+Olivergv1@users.noreply.github.com>
2023-07-28 19:55:19 -0500

    Uso de la lista Map para agregarle valores a cada keyword y mostrarla en pantalla

--- src/DetectorPhishing.java
+++ src/DetectorPhishing.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class DetectorPhishing {
@@ -10,60 +11,62 @@ public class DetectorPhishing {
         // Ruta del archivo a analizar
         String archivo = "D:\\Oliver\\UTM\\POO\\phishing.txt";
 
-        // Lista de palabras clave a buscar
+        // Se usa la lista Map para almacenar las palabras clave y sus valores
         
-        ArrayList<String> palabrasClave = new ArrayList<>();
-        palabrasClave.add("enlace");
-        palabrasClave.add("link");
-        palabrasClave.add("cuenta");
-        palabrasClave.add("contraseña");
-        palabrasClave.add("alerta");
-        palabrasClave.add("seguridad");
-        palabrasClave.add("premio");
-        palabrasClave.add("regalo");
-        palabrasClave.add("proteger");
-        palabrasClave.add("whatsapp");
-        palabrasClave.add("faceboock");
-        palabrasClave.add("instagram");
-        palabrasClave.add("netflix");
-        palabrasClave.add("amazon");
-        palabrasClave.add("twitter");
-        palabrasClave.add("google");
-        palabrasClave.add("paypal");
-        palabrasClave.add("banco");
-        palabrasClave.add("peligro");
-        palabrasClave.add("urgente");
-        palabrasClave.add("codigo");
-        palabrasClave.add("rembolso");
-        palabrasClave.add("gratis");
-        palabrasClave.add("cupon");
-        palabrasClave.add("descuento");
-        palabrasClave.add("verificar");
-        palabrasClave.add("felicidades");
-        palabrasClave.add("factura");
-        palabrasClave.add("copia");
-        palabrasClave.add("notificacion");
-        palabrasClave.add("expira en 3 dias");
-        palabrasClave.add("la contraseña de su cuenta ha caducado");
-        palabrasClave.add("se requiere verificacion");
-        palabrasClave.add("Su cuenta ha sido bloqueada");
+        Map<String, Integer> palabrasValores = new HashMap<>();
+        palabrasValores.put("enlace", 3);
+        palabrasValores.put("link", 3);
+        palabrasValores.put("cuenta", 3);
+        palabrasValores.put("contraseña", 3);
+        palabrasValores.put("alerta", 3);
+        palabrasValores.put("seguridad", 3);
+        palabrasValores.put("premio", 3);
+        palabrasValores.put("regalo", 3);
+        palabrasValores.put("proteger", 3);
+        palabrasValores.put("whatsapp", 2);
+        palabrasValores.put("faceboock", 3);
+        palabrasValores.put("instagram", 2);
+        palabrasValores.put("amazon", 2);
+        palabrasValores.put("twitter", 2);
+        palabrasValores.put("google", 3);
+        palabrasValores.put("paypal", 3);
+        palabrasValores.put("banco", 3);
+        palabrasValores.put("peligro", 3);
+        palabrasValores.put("urgente", 3);
+        palabrasValores.put("codigo", 3);
+        palabrasValores.put("rembolso", 3);
+        palabrasValores.put("gratis", 3);
+        palabrasValores.put("cupon", 3);
+        palabrasValores.put("descuento", 3);
+        palabrasValores.put("verificar", 3);
+        palabrasValores.put("felicidades", 1);
+        palabrasValores.put("factura", 1);
+        palabrasValores.put("copia", 1);
+        palabrasValores.put("notificacion", 1);
+        palabrasValores.put("expira en 3 dias", 2);
+        palabrasValores.put("la cotraseña de su cuenta de ha caducado", 3);
+        palabrasValores.put("se requiere verificacion", 3);
+        palabrasValores.put("su cuenta de ha sido bloqueada ", 3);
+        
 
-
-        // Se usa BufferedReader para leer el archivo y buscar las palabras clave
+        // se lee el archivo y se buscan las palabras clave
         try {
             BufferedReader br = new BufferedReader(new FileReader(archivo));
             String linea;
 
             while ((linea = br.readLine()) != null) {
-                for (String palabra : palabrasClave) {
+                for (Map.Entry<String, Integer> entry : palabrasValores.entrySet()) {
+                    String palabra = entry.getKey();
+                    int valor = entry.getValue();
                     if (linea.toLowerCase().contains(palabra.toLowerCase())) {
-                        System.out.println("Palabra clave encontrada: " + palabra);   //se imprime en panttalla la keyword encontrada
-                    }               
+                        System.out.println("Palabra clave encontrada: " + palabra + " (" + valor + " puntos)");   //se imprime en pantalla
+                    }
                 }
             }
-            br.close();   
+            br.close();
         } catch (IOException e) {
             System.out.println("Error al leer el archivo: " + e.getMessage());
         }
     }
 }
+
